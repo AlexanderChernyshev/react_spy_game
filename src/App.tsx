fd6c968ef33c953484800e036793ced53f9e8fd1
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2025-11-05 20:02:13 -0500

    state resulting in flippable tiles

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,41 @@ import { useState } from "react";
 import "./App.css";
 import type { ReactNode } from "react";
 
-const grid = [
+const initialGrid = [
   [false, false, false],
   [false, false, false],
   [false, false, false],
 ];
 
-const generatedGridCells: ReactNode[] = [];
+function App() {
+  const [grid, setGrid] = useState(initialGrid);
 
-for (let i = 0; i < grid.length; i++) {
-  for (let j = 0; j < grid[i].length; j++) {
-    if (grid[i][j]) {
-      generatedGridCells.push(<img src="/wrong_tile.svg" />);
-    } else {
-      generatedGridCells.push(<img src="/tile.svg" />);
+  const generatedGridCells: ReactNode[] = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j]) {
+        generatedGridCells.push(<img key={`${i}${j}`} src="/wrong_tile.svg" />);
+      } else {
+        generatedGridCells.push(
+          <img
+            key={`${i}${j}`}
+            src="/tile.svg"
+            onClick={() => {
+              flipTile(i, j);
+            }}
+          />
+        );
+      }
     }
   }
-}
 
-function App() {
+  function flipTile(i: number, j: number) {
+    const newGrid = [...grid];
+    newGrid[i][j] = !newGrid[i][j];
+    setGrid(newGrid);
+  }
+
   return (
     <>
       <h1>Where in the world is...</h1>
