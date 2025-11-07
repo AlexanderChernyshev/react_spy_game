24527d93291d45a1d47794a05d606ac52c628196
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2025-11-06 22:01:06 -0500

    Moving Spy
    Co-authored-by: Sergey Chernyshev <38076+sergeychernyshev@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,66 @@ function App() {
     }
   }
 
+  function spyCheckValidMoves(clickX: number, clickY: number): number[][] {
+    const validMoves = [];
+    const maxX = grid.length - 1;
+    const maxY = grid[0].length - 1;
+    const above = [spy[0], spy[1] - 1];
+    const left = [spy[0] - 1, spy[1]];
+    const right = [spy[0] + 1, spy[1]];
+    const below = [spy[0], spy[1] + 1];
+
+    if (
+      !(above[0] === clickX && above[1] === clickY) &&
+      above[1] >= 0 &&
+      above[1] <= maxY &&
+      !grid[above[0]][above[1]]
+    ) {
+      validMoves.push(above);
+    }
+
+    if (
+      !(left[0] === clickX && left[1] === clickY) &&
+      left[0] >= 0 &&
+      left[0] <= maxX &&
+      !grid[left[0]][left[1]]
+    ) {
+      validMoves.push(left);
+    }
+
+    if (
+      !(right[0] === clickX && right[1] === clickY) &&
+      right[0] >= 0 &&
+      right[0] <= maxX &&
+      !grid[right[0]][right[1]]
+    ) {
+      validMoves.push(right);
+    }
+
+    if (
+      !(below[0] === clickX && below[1] === clickY) &&
+      below[1] >= 0 &&
+      below[1] <= maxX &&
+      !grid[below[0]][below[1]]
+    ) {
+      validMoves.push(below);
+    }
+
+    return validMoves;
+  }
+
+  function spyConfirmMove(validMoves: number[][]) {
+    if (validMoves.length === 0) {
+      return spy;
+    }
+    return validMoves[Math.floor(Math.random() * validMoves.length)];
+  }
+
   function flipTile(i: number, j: number) {
     if (i === spy[0] && j === spy[1]) {
       setGameOver(true);
+    } else {
+      setSpyCoords(spyConfirmMove(spyCheckValidMoves(i, j)));
     }
 
     const newGrid = structuredClone(grid);
@@ -66,6 +123,10 @@ function App() {
       <h1>Where in the world is...</h1>
       <div className="grid">{generatedGridCells}</div>
       {gameOver ? <h2>You found the Spy!</h2> : null}
+      {/* This prints the spy's coords for debugging pruposes
+       <h2>
+        {spy[0]} : {spy[1]}
+      </h2> */}
       {gameOver ? <button onClick={restartGame}>Restart Game</button> : null}
     </>
   );
